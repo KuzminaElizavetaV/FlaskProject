0a5516f93de259a1783da143a676df28a587db2e
Елизавета Кузьмина <ekuzmina@TNEWS.local>
2023-09-08 16:53:56 +0300

    Домашнее задание семинара 1 дополнение

--- HW_Sem_01/app_1.py
+++ HW_Sem_01/app_1.py
@@ -68,6 +68,118 @@ def cloth():
     return render_template('cloth.html', **context, cloths=cloth_list)
 
 
+@app.route('/jackets/')
+def jackets():
+    context = {'title': 'Куртки'}
+    jackets_list = [
+        {
+            'name': 'Куртка_1',
+            'image': 'jacket_1.jpg',
+            'description': 'Женский пуховик, цвет: белый, размер S, сезон: зима'
+        },
+        {
+            'name': 'Куртка_2',
+            'image': 'jacket_2.jpg',
+            'description': 'Женский пуховик-пальто, цвет: розовый, размер М, сезон: весна, осень'
+        },
+        {
+            'name': 'Куртка_3',
+            'image': 'jacket_3.jpg',
+            'description': 'Мужская курта, цвет: серо-бежевый, размер М, сезон: весна, осень'
+        },
+        {
+            'name': 'Куртка_4',
+            'image': 'jacket_4.jpg',
+            'description': 'Мужской пуховик, цвет: бежевый, размер М, сезон: зима'
+        }
+    ]
+    return render_template('jackets.html', **context, jackets=jackets_list)
+
+
+@app.route('/dresses/')
+def dresses():
+    context = {'title': 'Платья'}
+    dresses_list = [
+        {
+            'name': 'Платье_1',
+            'image': 'dress_1.jpg',
+            'description': 'Платье черное, трикотаж, размер М'
+        },
+        {
+            'name': 'Платье_2',
+            'image': 'dress_2.jpg',
+            'description': 'Платье коктейльное в черный горох, размер S'
+        },
+        {
+            'name': 'Платье_3',
+            'image': 'dress_3.jpg',
+            'description': 'Платье с принтом Хризантемы, размер М'
+        },
+        {
+            'name': 'Платье_4',
+            'image': 'dress_4.jpeg',
+            'description': 'Платье летнее шифоновое с голубым принтом, размер S'
+        }
+    ]
+    return render_template('dresses.html', **context, dresses=dresses_list)
+
+
+@app.route('/jeans/')
+def jeans():
+    context = {'title': 'Джинсы'}
+    jeans_list = [
+        {
+            'name': 'Джинсы_1',
+            'image': 'jeans_1.jpg',
+            'description': 'Прямые женские джинсы, размер М'
+        },
+        {
+            'name': 'Джинсы_2',
+            'image': 'jeans_2.jpg',
+            'description': 'Молодежные джинсы, размер М'
+        },
+        {
+            'name': 'Джинсы_3',
+            'image': 'jeans_3.jpg',
+            'description': 'Женские черные джинсы, размер S'
+        },
+        {
+            'name': 'Джинсы_4',
+            'image': 'jeans_4.jpeg',
+            'description': 'Джинсы для девочки, рост 134-146 см'
+        }
+    ]
+    return render_template('jeans.html', **context, jeans=jeans_list)
+
+
+@app.route('/skirts/')
+def skirts():
+    context = {'title': 'Юбки'}
+    skirts_list = [
+        {
+            'name': 'Юбка_1',
+            'image': 'skirt_1.jpg',
+            'description': 'Юбка для девочки, рост 134-140'
+        },
+        {
+            'name': 'Юбка_2',
+            'image': 'skirt_2.jpeg',
+            'description': 'Юбка школьная для девочки-подростка, рост 140-146'
+        },
+        {
+            'name': 'Юбка_3',
+            'image': 'skirt_3.jpg',
+            'description': 'Длинная черная юбка, размер S'
+        },
+        {
+            'name': 'Юбка_4',
+            'image': 'skirt_4.jpg',
+            'description': 'Длинная шифоновая черная юбка, размер М'
+        }
+    ]
+    return render_template('skirts.html', **context, skirts=skirts_list)
+
+
 @app.route('/shoes/')
 def shoes():
     context = {'title': 'Обувь'}
